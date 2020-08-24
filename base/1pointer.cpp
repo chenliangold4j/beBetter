9922bf7388bfae3f9c502a271f6aaa81fbe000f7
chenliang <chenliangxingji1@163.com>
2020-08-24 19:31:21 +0800

    init

---
+++ base/1pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+//取地址
+int main ()
+{
+   int  var1;
+   char var2[10];
+ 
+   cout << "var1 address ";
+   cout << &var1 << endl;
+ 
+   cout << "var2 address ";
+   cout << &var2 << endl;
+ 
+   return 0;
+}
---
+++ base/1pointer2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+//  在变量声明的时候，如果没有确切的地址可以赋值，为指针变量赋一个 NULL 值是一个良好的编程习惯。
+
+int main ()
+{
+   int  var = 20;   // 实际变量的声明
+   int  *ip;        // 指针变量的声明
+ 
+   ip = &var;       // 在指针变量中存储 var 的地址
+ 
+   cout << "Value of var variable: ";
+   cout << var << endl;
+ 
+   // 输出在指针变量中存储的地址
+   cout << "Address stored in ip variable: ";
+   cout << ip << endl;
+ 
+   // 访问指针中地址的值
+   cout << "Value of *ip variable: ";
+   cout << *ip << endl;
+ 
+   return 0;
+}
